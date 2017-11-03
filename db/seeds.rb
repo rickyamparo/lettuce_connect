edf96999af4ff08f09b309d35951b9d1ffeea061
Ricky Amparo <rickyamparo@gmail.com>
2017-11-02 19:37:04 -0600

    add link to manage handles to navbar when user is logged in, add handletype creation to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,21 +4,25 @@ luke = User.create(first_name: "Luke", last_name: "Skywalker", email: "force@jed
 david = User.create(first_name: "David", last_name: "Lightman", email: "war@games.com", password: "whoppr")
 kevin = User.create(first_name: "Kevin", last_name: "Flynn", email: "tron@mcu.com", password: "disc")
 
-Handle.create(user: rick, name: 'rickdeckard')
-Handle.create(user: rick, name: 'rickdeckard')
-Handle.create(user: rick, name: 'rickdeckard')
+github = HandleType.create(name: 'github')
+twitter = HandleType.create(name: 'twitter')
+linkedin = HandleType.create(name: 'linkedin')
 
-Handle.create(user: luke, name: 'lukeskywalker')
-Handle.create(user: luke, name: 'lukeskywalker')
-Handle.create(user: luke, name: 'lukeskywalker')
+Handle.create(user: rick, name: 'rickdeckard', handle_type: github)
+Handle.create(user: rick, name: 'rickdeckard', handle_type: twitter)
+Handle.create(user: rick, name: 'rickdeckard', handle_type: linkedin)
 
-Handle.create(user: david, name: 'davidlightman')
-Handle.create(user: david, name: 'davidlightman')
-Handle.create(user: david, name: 'davidlightman')
+Handle.create(user: luke, name: 'lukeskywalker', handle_type: github)
+Handle.create(user: luke, name: 'lukeskywalker', handle_type: twitter)
+Handle.create(user: luke, name: 'lukeskywalker', handle_type: linkedin)
 
-Handle.create(user: kevin, name: 'kevinflynn')
-Handle.create(user: kevin, name: 'kevinflynn')
-Handle.create(user: kevin, name: 'kevinflynn')
+Handle.create(user: david, name: 'davidlightman', handle_type: github)
+Handle.create(user: david, name: 'davidlightman', handle_type: twitter)
+Handle.create(user: david, name: 'davidlightman', handle_type: linkedin)
+
+Handle.create(user: kevin, name: 'kevinflynn', handle_type: github)
+Handle.create(user: kevin, name: 'kevinflynn', handle_type: twitter)
+Handle.create(user: kevin, name: 'kevinflynn', handle_type: linkedin)
 
 Connection.create(user: rick, scanned_id: luke.id)
 Connection.create(user: rick, scanned_id: david.id)
