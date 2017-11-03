da8cb89f140ba30f31b359cf34345e66ab74af83
Ricky Amparo <rickyamparo@gmail.com>
2017-11-02 20:26:20 -0600

    finish styling connections page

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,8 +1,24 @@
 class Connection < ApplicationRecord
   belongs_to :user
 
-  def scanned_handles
-    Handle.where(user_id: self.scanned_id)
+  # def scanned_handles
+  #   Handle.where(user_id: self.scanned_id)
+  # end
+
+  def scanned_user
+    User.find(self.scanned_id)
+  end
+
+  def github_handle
+    Handle.find_by(user_id: self.scanned_id, handle_type_id: 1)
+  end
+
+  def twitter_handle
+    Handle.find_by(user_id: self.scanned_id, handle_type_id: 2)
+  end
+
+  def linkedin_handle
+    Handle.find_by(user_id: self.scanned_id, handle_type_id: 3)
   end
 
 end
