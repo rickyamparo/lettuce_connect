0dd6983ad3e55d29bfe1f32af36afa85feabb50c
Ricky Amparo <rickyamparo@gmail.com>
2017-11-04 12:23:50 -0600

    add model validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :handles
   has_many :connections
+
+  validates :first_name, :last_name, :password, presence:  true
+  validates :email, presence: true, uniqueness: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,5 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:first_name) }
   it { is_expected.to validate_presence_of(:last_name) }
   it { is_expected.to validate_uniqueness_of(:email) }
-  it { is_expected.to validate_length_of(:password).is_at_least(10) }
+  it { is_expected.to validate_presence_of(:password) }
 end
