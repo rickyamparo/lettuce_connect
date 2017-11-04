800afb3b8c10fc9110f7c939a5c352188e3a9117
Ricky Amparo <rickyamparo@gmail.com>
2017-11-04 12:18:35 -0600

    add shoulda matchers, create user mdoel test

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development, :test do
   gem 'pry'
   gem 'database_cleaner'
   gem 'factory_bot_rails'
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 group :development do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:last_name) }
+  it { is_expected.to validate_uniqueness_of(:email) }
+  it { is_expected.to validate_length_of(:password).is_at_least(10) }
+end
