be886f9af2767bfd2d5c79f1ec4b0b96ecfb8b66
Ricky Amparo <rickyamparo@gmail.com>
2017-11-04 13:35:35 -0600

    create github oauth spec

---
+++ spec/features/user_can_log_in_with_github_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature "User can view basic github information" do
+
+  before(:each) do
+    stub_omniauth
+  end
+
+  scenario "a user logs in and can view their basic github information" do
+    visit "/"
+    click_on "Sign in with Github"
+    expect(current_path).to eq("/")
+    expect(page).to have_content('Ricky Amparo')
+    expect(page).to have_content('rickyamparo@gmail.com')
+  end
+
+end
