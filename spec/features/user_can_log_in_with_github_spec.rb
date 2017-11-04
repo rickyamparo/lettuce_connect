e2fc3000a1fe1f840106e8a771ceec8e6bf97e40
Ricky Amparo <rickyamparo@gmail.com>
2017-11-04 14:43:32 -0600

    add sad path expectation to user login with github spec

--- spec/features/user_can_log_in_with_github_spec.rb
+++ spec/features/user_can_log_in_with_github_spec.rb
@@ -10,6 +10,7 @@ feature "User can view basic github information" do
     visit "/"
     click_on "Sign in with Github"
     expect(current_path).to eq("/")
+    expect(page).to_not have_content("Sign in with Github")
     expect(page).to have_content('Ricky Amparo')
     expect(page).to have_content('rickyamparo@gmail.com')
   end
