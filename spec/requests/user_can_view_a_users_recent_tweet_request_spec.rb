201626e2af22949f7b7a4a3aaed2bfb2a828619f
Ricky Amparo <rickyamparo@gmail.com>
2017-11-06 17:37:30 -0700

    create twitter api consumption spec

---
+++ spec/requests/user_can_view_a_users_recent_tweet_request_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'View a Users recent tweet' do
+  scenario "A user goes to a connection and views recent tweet by that user" do
+    user = create(:user)
+    user_2 = create(:user, first_name: "Richard", email: "blade@runner2.com")
+
+    github = HandleType.create(name: "Github")
+    twitter = HandleType.create(name: "Twitter")
+    linkedin = HandleType.create(name: "LinkedIn")
+
+    Handle.create(user: user_2, name: 'rickyamparo', handle_type: github)
+    Handle.create(user: user_2, name: 'rickyaceamparo', handle_type: twitter)
+    Handle.create(user: user_2, name: 'fakelinkedin2', handle_type: linkedin)
+
+    Connection.create(user: user, scanned_id: user_2.id)
+
+    visit '/'
+    click_on ("Login")
+    fill_in "session[email]", with: "blade@runner.com"
+    fill_in "session[password]", with: "replicants"
+    click_on ("Submit")
+
+    visit'/connections'
+    click_on("Richard")
+
+    within ('.twitter-handle') do
+      expect(page).to have_css('.twitter-event')
+    end
+  end
+end
