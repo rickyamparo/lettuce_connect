35403d8d0377afc8d40888f965eff3e1844c5a22
Ricky Amparo <rickyamparo@gmail.com>
2017-11-07 11:17:12 -0700

    create twitter method calls in the connection controller show action

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -7,6 +7,8 @@ class ConnectionsController < ApplicationController
 
   def show
     @connection = Connection.find(params[:id])
+    raw_recent_tweet = TwitterService.recent_tweet(@connection)
+    @recent_tweet = Tweet.recent_tweet(raw_recent_tweet)
     raw_github_events = GithubService.recent_events(@connection)
     @github_events = GithubEvent.recent_events(raw_github_events.first(3))
   end
