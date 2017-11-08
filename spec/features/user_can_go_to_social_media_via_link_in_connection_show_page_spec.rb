8c2ab9c02fc6b143be08652e35d7a2490d3315a2
Ricky Amparo <rickyamparo@gmail.com>
2017-11-08 08:50:50 -0700

    add expectations and setup to tests

--- spec/features/user_can_go_to_social_media_via_link_in_connection_show_page_spec.rb
+++ spec/features/user_can_go_to_social_media_via_link_in_connection_show_page_spec.rb
@@ -1,1 +1,33 @@
 require 'rails_helper'
+
+feature "Handle name link" do
+  it "a user can click on the handle name in the conenctions show" do
+    user = create(:user)
+    user_2 = create(:user, first_name: "Richard", email: "blade@runner2.com")
+
+    github = HandleType.create(name: "Github")
+    twitter = HandleType.create(name: "Twitter")
+    linkedin = HandleType.create(name: "LinkedIn")
+
+    Handle.create(user: user_2, name: 'rickyamparo', handle_type: github)
+    Handle.create(user: user_2, name: 'rickyaceamparo', handle_type: twitter)
+    Handle.create(user: user_2, name: 'fakelinkedin2', handle_type: linkedin)
+
+    Connection.create(user: user, scanned_id: user_2.id)
+
+    visit '/'
+    click_on ("Login")
+    fill_in "session[email]", with: "blade@runner.com"
+    fill_in "session[password]", with: "replicants"
+    click_on ("Submit")
+
+    visit'/connections'
+    click_on 'Richard'
+
+    expect(page).to have_link("rickyamparo")
+    expect(page).to have_link("rickyaceamparo")
+
+    click_on("rickyamparo")
+    expect(current_path).to be('github')
+  end
+end
