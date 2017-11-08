4ebd766a73060d99068ca27648ff0df36c302ac9
Ricky Amparo <rickyamparo@gmail.com>
2017-11-08 12:42:59 -0700

    add simplecov and unskipped skipped tests

--- spec/features/api/user_can_view_a_users_recent_github_events_request_spec.rb
+++ spec/features/api/user_can_view_a_users_recent_github_events_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'View a Users github events' do
-  xit "A user goes to a connection and views github info" do
+  it "A user goes to a connection and views github info" do
     user = create(:user)
     user_2 = create(:user, first_name: "Richard", email: "blade@runner2.com")
 
